7d3d512e5d868038b784742d2856991df5a18b2c
jamie-codez <cruiseomondi90@gmail.com>
2023-06-18 15:25:47 +0300

    Initial commit

---
+++ src/test/kotlin/com/code/jamie/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.code.jamie
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import com.code.jamie.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
